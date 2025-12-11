2df667bfd10e347304ebcea1203e2992b8151d4e
Evan Johnson <ev.jonnson@gmail.com>
2025-12-11 01:43:41 -0700

    free/sync switching functionality working
    syncing not great at very slow speeds

--- silhouette-rotator.lua
+++ silhouette-rotator.lua
@@ -1,21 +1,29 @@
+-- CONFIGURATION VARIABLES
+clock_in_div = 1/4
 low = -5.0
 high = 5.1
-range = high - low
-time_min = .045
+-- min and max time for a cycle (a.k.a. spin speed, min time is max speed)
+-- the parameter scaling for time is tuned for these values
+-- likely will want to change time_parameter_handler if you change these
+time_min = .045 -- speeds faster than this will start clicking
 time_max = 30
+
+-- CONVENIENCE VARIABLES
+range = high - low
 time_range = time_max - time_min
 step_size = 0.01
 steps = math.floor((range) / step_size) - 1
-sync = true
+tempo = clock.tempo
+beat_sec = clock.get_beat_sec()
+
+-- STATE VARIABLES
+sync = false
 sync_div = 1/1
 sync_offset = 0
 sync_step = 0
 syncer_id = -1
--- sync_error_adjuster = 1 -- when synced, time gets multiplied by this
-beat_sec = clock.get_beat_sec()
-tempo = clock.tempo
 
--- MATH UTILS
+-- UTILITIES
 local function truncate(num)
     return math.floor(num * 1000) / 1000
 end
@@ -24,12 +32,21 @@ local function clamp(n, min, max)
     return math.max(min, math.min(max, n))
 end
 
+local function biased_curve(p, center, lower_exponent, upper_exponent)
+    if p < center then
+        return center * ((p / center) ^ lower_exponent)
+    else
+        return 1 - (1 - center) * (((1 - p) / (1 - center)) ^ upper_exponent)
+    end
+end
+
+-- CLOCKWORKS
 function await_clock()
     input[2].mode( 'change', 3, 0.1, 'rising' )
     input[2].change = function()
-        input[2].mode( 'clock', 1/4)
+        input[2].mode( 'clock', clock_in_div)
         sync = true
-        synced_rotation()
+        spin_synced()
         syncer_id = clock.run(syncer)
         clock_timeout_checker:start()
     end
@@ -37,9 +54,9 @@ end
 
 clock_timeout_checker = metro.init{
     event = function()
-        if clock.get_last_input_time() > 4 then -- 4 second timeout
-            clock_timeout:stop()
-            free_rotation()
+        if clock.time_since_last_input() > 4 then -- 4 second timeout
+            clock_timeout_checker:stop()
+            spin_free()
             await_clock()
         end
     end,
@@ -50,72 +67,66 @@ clock_timeout_checker = metro.init{
 clock.handlers.tempo_change = function(new_tempo)
     beat_sec = clock.get_beat_sec()
 
-    -- if math.abs(new_tempo - tempo) > .3 then -- ignore spurious tempo changes
     if math.abs(1 - (new_tempo / tempo)) > .01 then -- ignore spurious tempo changes
         print("CLOCK CHANGE: "..tempo.." -> "..new_tempo)
-        update_offset()
+        -- update_offset()
     end
 
     tempo = new_tempo
 end
 
-output[4]({
-    to(5, 0),
-    to(5, 0.05),
-    to(0, 0)
-})
+-- output[4]({
+--     to(5, 0),
+--     to(5, 0.05),
+--     to(0, 0)
+-- })
 
 function syncer()
     while true do
         clock.sync(sync_div, sync_offset)
-        output[4]()
+        -- output[4]()
 
         -- adjust oscillator time to account for error
-        -- if dir == 1 then -- voltage is falling
-           local step_error = sync_step - output[1].dyn.step
-           local speed_error = step_error / steps * -1 * output[1].dyn.dir
-           -- error greater than this is likely incorrect
-           -- speed_error = clamp(speed_error, -.2, .2)
-           local sync_error_adjuster = 1 + speed_error
-           output[1].dyn.sync_error_adjuster = sync_error_adjuster
-           -- print(sync_error_adjuster)
-        -- else
-        -- end
+       local step_error = sync_step - output[1].dyn.step
+       local speed_error = step_error / steps * -1 * output[1].dyn.dir
+
+       -- error greater than this is likely incorrect
+       -- TODO this caused problems, maybe a timer that implements it after
+       -- the rotation stabilizes?
+       -- speed_error = clamp(speed_error, -.2, .2)
+
+       local sync_error_adjuster = 1 + speed_error
+       output[1].dyn.sync_error_adjuster = sync_error_adjuster
     end
 end
 
-function update_offset()
-    print('UPDATING OFFSET')
+function new_syncer()
     clock.cancel(syncer_id)
     syncer_id = clock.run(syncer)
 
     local current_beat = clock.get_beats()
     sync_offset = current_beat % sync_div
     sync_step = output[1].dyn.step
+    output[1].dyn.sync_error_adjuster = 1
 end
 
-function update_sync_div(div)
-    sync_div = div
-    update_offset()
+function spin_free()
+    sync = false
+    clock.cancel(syncer_id)
+    syncer_id = -1
+
+    input[1].mode( 'stream', 0.001 )
+    output[1].dyn.sync_error_adjuster = 1
 end
 
-function free_rotation()
-    -- output[1](spinner)
-    output[1].dyn.sync_stepper = 0
-    output[1].dyn.sync_step = steps
-end
+function spin_synced()
+    sync = true
+    input[1].mode( 'stream', 0.01 )
 
-function synced_rotation()
-    output[1].dyn.run = 0
-    output[1].dyn.step = 0
-    output[1].dyn.t = beat_sec * sync_div
-    output[1].dyn.run = 1
-    -- output[1].dyn.sync_step = steps
-    -- output[1].dyn.sync_stepper = -1
-    -- output[1]()
-
-    update_offset()
-    -- syncer_id = clock.run(syncer)
+    if output[1].dyn.run == 1 then
+        output[1].dyn.t = beat_sec * sync_div
+        new_syncer()
+    end
 end
 
 -- p is 0-1, maps to time range
@@ -128,57 +139,68 @@ function update_time_free(p, dir, run)
     output[1].dyn.run = run
 end
 
-function update_time_synced(p, dir, run)
-    -- output[1].dyn.t = (beat_sec * sync_div) * sync_error_adjuster
-    output[1].dyn.t = (beat_sec * sync_div)
-end
-
-spinner = loop{
-    asl._while(dyn{step = steps+1}:step(dyn{dir = -1}):wrap(0, steps+1), {
-        asl._if((dyn{run=0}), {
-            to(low + (dyn{step = steps+1} * step_size), ((dyn{t = 0.5} / steps) * dyn{sync_error_adjuster = 1}))
-        }),
-        asl._if((1 - (dyn{run=0})), {
-            to(low + (dyn{step = steps+1}:step(dyn{dir = -1} * -1) * step_size), 0.001)
-        })
-    }),
-    -- falling
-    asl._if(1 - dyn{dir = -1}, {
-        to(low, 0),
-        to(high, 0),
-    }),
-    -- rising
-    asl._if(dyn{dir = -1}, {
-        to(high, 0),
-        to(low, 0),
-    }),
+div_table = {
+    16/1, -- 0 - .25
+    8/1, -- .25 - .50
+    6/1, -- .50 - .75
+    4/1, -- .75 - 1.00
+    3/1, -- 1.00 - 1.25
+    2/1, -- 1.25 - 1.50
+    3/2, -- 1.50 - 1.75
+    4/3, -- 1.75 - 2.00
+    5/4, -- 2.00 - 2.25
+    1/1, -- 2.25 - 2.50
+    1/1, -- 2.50 - 2.75
+    4/5, -- 2.75 - 3.00
+    3/4, -- 3.00 - 3.25
+    2/3, -- 3.25 - 3.50
+    1/2, -- 3.50 - 3.75
+    1/3, -- 3.75 - 4.00
+    1/4, -- 4.00 - 4.25
+    1/6, -- 4.25 - 4.50
+    1/8, -- 4.50 - 4.75
+    1/16, -- 4.75 - 5.00
 }
 
--- spinner = loop{
---     asl._while(dyn{step = steps+1}:step(dyn{dir = -1}):wrap(0, steps+1), {
---         asl._if((dyn{run=0} * dyn{sync_step=steps}:step(dyn{sync_stepper=0})), {
---             to(low + (dyn{step = steps+1} * step_size), (dyn{t = 0.5} / steps))
---         }),
---         asl._if((1 - (dyn{run=0} * dyn{sync_step=steps})), {
---             to(low + (dyn{step = steps+1}:step(dyn{dir = -1} * -1) * step_size), 0.001)
---         })
---     }),
---     -- falling
---     asl._if(1 - dyn{dir = -1}, {
---         to(low, 0),
---         to(high, 0),
---     }),
---     -- rising
---     asl._if(dyn{dir = -1}, {
---         to(high, 0),
---         to(low, 0),
---     }),
--- }
+function update_time_synced(p, dir, run)
+    local idx = math.ceil(p * 20)
+    local div = div_table[idx] -- 5/19 = 0.26315
+    local running = output[1].dyn.run
+    local current_dir = output[1].dyn.dir
+    local syncer_dirty = false
 
--- output[1](spinner)
+    if run == 1 and running == 0 then
+        -- starting
+        output[1].dyn.run = 1
+        syncer_dirty = true
+    elseif run == 0 then
+        if running == 1 then
+            -- stopping
+            clock.cancel(syncer_id)
+            syncer_id = -1
+            output[1].dyn.run = 0
+        end
 
-input[1].mode( 'stream', 0.01 )
-input[1].stream = function(volts)
+        return
+    end
+
+    if dir ~= current_dir then
+        output[1].dyn.dir = dir
+        syncer_dirty = true
+    end
+
+    if div ~= sync_div then
+        output[1].dyn.t = (beat_sec * sync_div)
+        sync_div = div
+        syncer_dirty = true
+    end
+
+    if syncer_dirty then
+        new_syncer()
+    end
+end
+
+function time_parameter_handler(volts)
     local p = volts / 5
     p = truncate(p)
     p = clamp(p, -1, 1)
@@ -195,7 +217,7 @@ input[1].stream = function(volts)
 
     if not sync then
         -- p = p^3
-        p = biased_curve(p, 0.05, 2, 3)
+        p = biased_curve(p, 0.05, 2, 3) -- maybe not great for CV
 
         update_time_free(p, dir, run)
     else
@@ -203,21 +225,36 @@ input[1].stream = function(volts)
     end
 end
 
--- maybe not great for cv
-function biased_curve(p, center, lower_exponent, upper_exponent)
-    if p < center then
-        return center * ((p / center) ^ lower_exponent)
-    else
-        return 1 - (1 - center) * (((1 - p) / (1 - center)) ^ upper_exponent)
-    end
-end
-
 function init()
-    output[1].action = spinner
-    output[1]()
+    local spinner = loop{
+        asl._while(dyn{step = steps+1}:step(dyn{dir = -1}):wrap(0, steps+1), {
+            asl._if((dyn{run=0}), {
+                to(low + (dyn{step = steps+1} * step_size), ((dyn{t = 0.5} / steps) * dyn{sync_error_adjuster = 1}))
+            }),
+            asl._if((1 - (dyn{run=0})), {
+                to(low + (dyn{step = steps+1}:step(dyn{dir = -1} * -1) * step_size), 0.001)
+            })
+        }),
+        -- falling
+        asl._if(1 - dyn{dir = -1}, {
+            to(low, 0),
+            to(high, 0),
+        }),
+        -- rising
+        asl._if(dyn{dir = -1}, {
+            to(high, 0),
+            to(low, 0),
+        }),
+    }
 
-    -- free_rotation()
-    -- await_clock()
-    synced_rotation()
-    input[2].mode( 'clock', 1/2)
+    -- output[1].action = spinner
+    -- output[1]()
+    output[1](spinner)
+
+    input[1].stream = time_parameter_handler
+
+    spin_free()
+    await_clock()
+    -- synced_spin()
+    -- input[2].mode( 'clock', 1/2)
 end
